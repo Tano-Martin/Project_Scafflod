400a42cc1ee8bd0c45d5e2940f7da1fbf81edb01
Tano Martin <tanomartin3@gmail.com>
2021-05-01 19:11:43 +0000

    Affichage de template index

--- scafflod/urls.py
+++ scafflod/urls.py
@@ -14,14 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls.conf import include
+from .views import index, portfolio
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name="index"),
+    path('portfolio-details.html/', portfolio, name="portfolio"),
+    
 ]
+
 
 if settings.DEBUG :
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
