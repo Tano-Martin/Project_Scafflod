8080001fb4f1434a5164c756f3e6370df954aca3
Tano Martin <tanomartin3@gmail.com>
2021-05-01 01:21:31 +0000

    Creation d'app website

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,91 @@
 from django.contrib import admin
+from . import models
+from django.utils.safestring import mark_safe
 
 # Register your models here.
+
+@admin.register(models.About)
+class AboutAdmin(admin.ModelAdmin):
+        # Liste des champs a afficher
+    list_display = ('title', 'subtitle', 'picture', 'date_add', 'date_update', 'status')
+
+        # Configuration du champ de recherche
+    search_fields = ('title', 'date_add')
+
+    def images_view(self, obj): #Permet d'avoir un aperçu des images
+        return mark_safe('<img src="{obj.image.url}" style="height:50px; width:100px">'.format(url=obj.image.url))
+
+    images_view.short_description = 'Aperçu des images' # Titre de l'onglet dans l'admin
+
+@admin.register(models.OptionAbout)
+class OptionAboutAdmin(admin.ModelAdmin):
+        # Liste des champs a afficher
+    list_display = ('description', 'date_add', 'date_update', 'status')
+
+        # Configuration du champ de recherche
+    search_fields = ('description', 'date_add')
+
+
+@admin.register(models.Banner)
+class BannerAdmin(admin.ModelAdmin):
+        # Liste des champs a afficher
+    list_display = ('title', 'description', 'picture', 'date_add', 'date_update', 'status')
+
+        # Configuration du champ de recherche
+    search_fields = ('title', 'date_add')
+
+    def images_view(self, obj): #Permet d'avoir un aperçu des images
+        return mark_safe('<img src="{obj.image.url}" style="height:50px; width:100px">'.format(url=obj.image.url))
+
+    images_view.short_description = 'Aperçu des images' # Titre de l'onglet dans l'admin
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+        #liste des champs a afficher
+    list_display = ('name', 'email', 'subject', 'message', 'date_add', 'date_update', 'status')
+
+        # Configuration du champ de recherche
+    search_fields = ('name', 'date_add')
+
+@admin.register(models.Newletter)
+class NewletterAdmin(admin.ModelAdmin):
+        #liste des champs a afficher
+    list_display = ('email', 'date_add', 'date_update', 'status')
+
+        # Configuration du champ de recherche
+    search_fields = ('email', 'date_add')
+
+@admin.register(models.Website)
+class WebsiteAdmin(admin.ModelAdmin):
+        #liste des champs a afficher
+    list_display = ('name_site', 'description_service', 'description_portfolio', 'call_action', 'description_testimonial', 'description_team', 'description_client', 'description_pricing', 'location', 'email', 'contact', 'maps', 'copyrights', 'date_add', 'date_update', 'status')
+
+        # Configuration du champ de recherche
+    search_fields = ['date_add']
+
+
+@admin.register(models.SocialIcon)
+class SocialIconAdmin(admin.ModelAdmin):
+        # Liste des champs a afficher
+    list_display = ('name', 'lien', 'icon', 'date_add', 'date_update', 'status')
+
+        # Configuration du champ de recherche
+    search_fields = ('name', 'date_add')
+
+    def images_view(self, obj): #Permet d'avoir un aperçu des images
+        return mark_safe('<img src="{obj.image.url}" style="height:50px; width:100px">'.format(url=obj.image.url))
+
+    images_view.short_description = 'Aperçu des images' # Titre de l'onglet dans l'admin
+
+@admin.register(models.Team)
+class TeamAdmin(admin.ModelAdmin):
+        # Liste des champs a afficher
+    list_display = ('name', 'poste', 'picture', 'twitter', 'facebook', 'linkedin', 'instagram', 'date_add', 'date_update', 'status')
+
+        # Configuration du champ de recherche
+    search_fields = ('name', 'date_add')
+
+    def images_view(self, obj): #Permet d'avoir un aperçu des images
+        return mark_safe('<img src="{obj.image.url}" style="height:50px; width:100px">'.format(url=obj.image.url))
+
+    images_view.short_description = 'Aperçu des images' # Titre de l'onglet dans l'admin
--- website/models.py
+++ website/models.py
@@ -1,3 +1,151 @@
 from django.db import models
 
 # Create your models here.
+class About(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.TextField()
+    picture = models.FileField()
+    option = models.ManyToManyField("website.OptionAbout", related_name="Option_about")
+
+        # Champs obligatoires (Convention de NaN)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta():
+        verbose_name = 'About'
+        verbose_name_plural = 'Abouts'
+
+    def __str__(self):
+        return self.title
+
+
+class OptionAbout(models.Model):
+    description = models.CharField(max_length=255)
+
+        # Champs obligatoires (Convention de NaN)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta():
+        verbose_name = 'OptionAbout'
+        verbose_name_plural = 'OptionAbouts'
+
+    def __str__(self):
+        return self.description
+
+class Banner(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    picture = models.FileField()
+
+        # Champs obligatoires (Convention de NaN)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta():
+        verbose_name = 'Banner'
+        verbose_name_plural = 'Banners'
+
+    def __str__(self):
+        return self.title
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+
+        # Champs obligatoires (Convention de NaN)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta():
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+
+    def __str__(self):
+        return self.name
+
+class Newletter(models.Model):
+    email = models.EmailField()
+
+        # Champs obligatoires (Convention de NaN)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta():
+        verbose_name = 'Newletter'
+        verbose_name_plural = 'Newletters'
+
+    def __str__(self):
+        return self.email
+
+class Website(models.Model):
+    name_site = models.CharField(max_length=255)
+    description_service = models.TextField()
+    description_portfolio = models.TextField()
+    call_action = models.TextField()
+    description_testimonial = models.TextField()
+    description_team = models.TextField()
+    description_client = models.TextField()
+    description_pricing = models.TextField()
+    location = models.CharField(max_length=255)
+    email = models.EmailField()
+    contact = models.CharField(max_length=255)
+    maps = models.FileField()
+    copyrights = models.CharField(max_length=255)
+
+        # Champs obligatoires (Convention de NaN)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta():
+        verbose_name = 'Website'
+        verbose_name_plural = 'Websites'
+
+    def __str__(self):
+        return self.name_site
+
+class SocialIcon(models.Model):
+    name = models.CharField(max_length=255)
+    lien = models.CharField(max_length=255)
+    icon = models.FileField()
+
+        # Champs obligatoires (Convention de NaN)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta():
+        verbose_name = 'SocialIcon'
+        verbose_name_plural = 'SocialIcons'
+
+    def __str__(self):
+        return self.name
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    poste = models.CharField(max_length=255)
+    picture = models.FileField()
+    twitter = models.CharField(max_length=255)
+    facebook = models.CharField(max_length=255)
+    linkedin = models.CharField(max_length=255)
+    instagram = models.CharField(max_length=255)
+
+        # Champs obligatoires (Convention de NaN)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta():
+        verbose_name = 'Team'
+        verbose_name_plural = 'Teams'
+
+    def __str__(self):
+        return self.name
